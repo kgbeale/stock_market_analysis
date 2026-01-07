0bd425e6797a021e58f6d128dc1c2ad06797e070
Kyle Beale <kyle.beale.20@cnu.edu>
2026-01-07 00:27:36 -0500

    Began work on time delay in javascript

--- api_calls.js
+++ api_calls.js
@@ -1,15 +1,14 @@
-'use strict';
 let request = require('request');
 
 let api_key = "STNE15X0T16UDA4F";
 let array = ["SPY","QQQ","IAU", "VONE", "VONG", "VONV", "IWM", "IEF"];
-let url = `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=IBM&apikey={api_key}`;
+let url = `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=SPY&apikey=${api_key}`;
 
 // Function to delay execution
 const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
 // Sequential execution with delays
-let promies = array.map(async(Element, index) => {
+let promises = array.map(async(ele, index) => {
   // Wait for (index * 800ms) before making the call
   // This spaces out calls by 0.8 seconds
   await delay(index * 800);
